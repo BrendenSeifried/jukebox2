42012ab4d31d01f58dc6d16329f61c6897a837c3
BrendenSeifried <brendenseifried@gmail.com>
2021-12-08 10:41:30 -0800

    first

---
+++ app.js
@@ -0,0 +1,20 @@
+const audio = document.getElementById('audio');
+
+const ninjaSound = document.getElementById('ninja-sound');
+ninjaSound.addEventListener('click', () => {
+  audio.src = 'assets/sounds/alarm.wav';
+  audio.play();
+});
+
+const princessSound = document.getElementById('princess-sound');
+princessSound.addEventListener('click', () => {
+  audio.src = 'assets/sounds/princess.mp3';
+  audio.play();
+});
+
+const rangerSound = document.getElementById('ranger-sound');
+rangerSound.addEventListener('click', () => {
+  audio.src = 'assets/sounds/ranger.mp3';
+  audio.play();
+});
+
