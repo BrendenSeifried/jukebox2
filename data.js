1468fd06a2109f62ccfa91d0187852467bd3d6c2
BrendenSeifried <brendenseifried@gmail.com>
2021-12-08 12:22:32 -0800

    import-1

--- data.js
+++ data.js
@@ -10,6 +10,26 @@ export const pics = [{
     name:'retro',
     image:'assets/avatars/space.png',
     audio:'assets/sounds/retro.wav',
+}, {
+    name:'dog',
+    image:'assets/avatars/dog.png',
+    audio:'assets/sounds/dog.wav',
+}, {
+    name:'bird',
+    image:'assets/avatars/bird.png',
+    audio:'assets/sounds/bird.wav',
+}, {
+    name:'money',
+    image:'assets/avatars/money.png',
+    audio:'assets/sounds/money.wav',
+}, {
+    name:'boat',
+    image:'assets/avatars/boat.png',
+    audio:'assets/sounds/boat.wav',
+}, {
+    name:'pond',
+    image:'assets/avatars/pond.png',
+    audio:'assets/sounds/pond.wav',
 }
 
 ];
