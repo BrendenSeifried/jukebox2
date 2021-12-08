e6c31d50f9fb7cef01e11f4a4d1ca36c3a43d660
BrendenSeifried <brendenseifried@gmail.com>
2021-12-08 12:20:29 -0800

    data.js

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
+import { pics } from './data.js';
+
+console.log(pics);
+
 const audio = document.getElementById('audio');
 
-const ninjaSound = document.getElementById('ninja-sound');
+const ninjaSound = document.getElementById('alarm');
 ninjaSound.addEventListener('click', () => {
   audio.src = 'assets/sounds/alarm.wav';
   audio.play();
 });
 
-const princessSound = document.getElementById('princess-sound');
+const princessSound = document.getElementById('retro');
 princessSound.addEventListener('click', () => {
   audio.src = 'assets/sounds/retro.wav';
   audio.play();
 });
 
-const rangerSound = document.getElementById('ranger-sound');
+const rangerSound = document.getElementById('dog');
 rangerSound.addEventListener('click', () => {
   audio.src = 'assets/sounds/dog.wav';
   audio.play();
---
+++ data.js
@@ -0,0 +1,15 @@
+export const pics = [{
+    name:'jester',
+    image:'assets/avatars/jester.png',
+    audio:'assets/sounds/jester.wav',
+}, {
+    name:'alarm',
+    image:'assets/avatars/alarm.png',
+    audio:'assets/sounds/alarm.wav',
+}, {
+    name:'retro',
+    image:'assets/avatars/space.png',
+    audio:'assets/sounds/retro.wav',
+}
+
+];
